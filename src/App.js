bb850e1e511f41e555550af82c1e0b0a17108636
Toshihiko Komuro <toshihiko.komuro.jp@gmail.com>
2019-12-21 20:56:05 +0900

    add style and json place holder

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {cats} from './cats';
-
+import './App.css'
 
 
 class App extends Component  {
     constructor(){
     super()
     this.state = {
-        cats: cats,
+        cats: [],
         searchfield:''
     }
+    }
+
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ cats:cats }));
     }
 
     onSearchChange = (event) => {
@@ -22,13 +28,18 @@ class App extends Component  {
         const filterdCats = this.state.cats.filter(cat => {
             return cat.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return(
-            <div className='tc'>
-                <h1>CatFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList cats={filterdCats} />
-            </div>
-        );
+        if (this.state.cats.length === 0) {
+            return <h1>Loading</h1>  
+        } else {
+            return(
+           
+                <div className='tc'>
+                    <h1 className="f1">CatFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList cats={filterdCats} />
+                </div>
+            );
+        }
     }
 }
 
