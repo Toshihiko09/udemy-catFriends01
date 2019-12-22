401c7cb3d95f234bed78cad976999f124981bacf
Toshihiko Komuro <toshihiko.komuro.jp@gmail.com>
2019-12-22 17:13:29 +0900

    organized files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import {cats} from './cats';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css'
 
 
 class App extends Component  {
     constructor(){
-    super()
-    this.state = {
-        cats: [],
-        searchfield:''
-    }
+        super()
+        this.state = {
+            cats: [],
+            searchfield:''
+        }
     }
 
     componentDidMount(){
       fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({ cats:cats }));
+        .then(users => this.setState({ cats:users }));
     }
 
     onSearchChange = (event) => {
@@ -26,13 +25,13 @@ class App extends Component  {
     }
 
     render(){
-        const filterdCats = this.state.cats.filter(cat => {
-            return cat.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {cats, searchfield } = this.state;
+        const filterdCats = cats.filter(cat => {
+            return cat.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.cats.length === 0) {
-            return <h1>Loading</h1>  
-        } else {
-            return(
+        return !cats.length?
+            <h1>Loading</h1> :
+            (
            
                 <div className='tc'>
                     <h1 className="f1">CatFriends</h1>
@@ -44,6 +43,5 @@ class App extends Component  {
             );
         }
     }
-}
 
 export default App;
